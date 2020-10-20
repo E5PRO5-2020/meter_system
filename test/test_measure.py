8d161030fe4983f475ebb5c0e71fdae493951242
JakobVestergaard <34288208+JakobVestergaard@users.noreply.github.com>
2020-10-20 14:08:51 +0200

    Update test_measure.py
    Changed to the right version of the document

--- test/test_measure.py
+++ test/test_measure.py
@@ -1,6 +1,7 @@
+
 import pytest
-from meter.MeterMeasurement import Measurement, MeterMeasurement
-from meter.OmniPower import C1Telegram, OmniPower
+from OmniPower.MeterMeasurement import Measurement, MeterMeasurement
+from OmniPower.OmniPower import C1Telegram, OmniPower
 from binascii import hexlify
 from datetime import datetime
 import json
@@ -11,13 +12,13 @@ def MeasureFix():
 	m1 = Measurement(7, "kWh")
 	m2 = Measurement(8, "kWh")
 	m3 = Measurement(9, "kW")
-	m4 = Measurement(10, "kW")
-	m5 = Measurement(10000, "MWh")
-	m6 = Measurement("abcd", "123")
-	meterID = "3232323"
-	testTime = (2020, 10, 15, 15, 12, 30, 262939)
-	return m1, m2, m3, m4, m5, m6, meterID, testTime
+	m4 = Measurement("", "")
 
+	meterID = "3232323"
+	testdatetime = datetime(2020, 10, 15, 15, 12, 30, 262939)
+	return m1, m2, m3, m4, meterID, testdatetime
+
+# Setup fixture for keys
 @pytest.fixture()
 def keys():
 	k1 = "A+"
@@ -26,36 +27,32 @@ def keys():
 	k4 = "P-"
 	return k1, k2, k3, k4
 
-
+# Test for MeterMeasurement.Measure
 def test_measure(MeasureFix):
 	# Load in fixture
-	m1, m2, m3, m4, m5, m6, meterID, testTime = MeasureFix
+	m1, m2, m3, m4, meterID, testdatetime = MeasureFix
 	# Test with fixture-values
 	assert Measurement.__repr__(m1)=="7 kWh"
 	assert Measurement.__repr__(m2)=="8 kWh"
 	assert Measurement.__repr__(m3)=="9 kW"
-	assert Measurement.__repr__(m4)=="10 kW"
-	assert Measurement.__repr__(m5)=="10000 MWh"
-	assert Measurement.__repr__(m6)=="abcd 123"
+	assert Measurement.__repr__(m4)==" "
 
+	print("dinmor")
 	assert Measurement.__iter__(m1)== (m1.value, m1.unit)
 	assert Measurement.__iter__(m2)== (m2.value, m2.unit)
 	assert Measurement.__iter__(m3)== (m3.value, m3.unit)
 	assert Measurement.__iter__(m4)== (m4.value, m4.unit)
-	assert Measurement.__iter__(m5)== (m5.value, m5.unit)
-	assert Measurement.__iter__(m6)== (m6.value, m6.unit)
-
-
-
-
 
 def test_MeterMeasure(MeasureFix, keys):
+
+	# Currently the def __repr__(self):-function is not tested in MeterMeasurement.py
+
 	# Load in fixture
-	m1, m2, m3, m4, m5, m6, meterID, testTime = MeasureFix
+	m1, m2, m3, m4, meterID, testdateTime = MeasureFix
 	k1, k2, k3, k4 = keys
 	# Test with fixture-values
 	# Instantiate MeterMeasurement object and add values
-	omni_power_frame = MeterMeasurement(meterID, testTime)
+	omni_power_frame = MeterMeasurement(meterID, testdateTime)
 	omni_power_frame.add_measurement(k1, m1)
 	omni_power_frame.add_measurement(k2, m2)
 	omni_power_frame.add_measurement(k3, m3)
@@ -73,7 +70,24 @@ def test_MeterMeasure(MeasureFix, keys):
 	assert omni_power_frame.Measurements[k4].value == m4.value
 	assert omni_power_frame.Measurements[k4].unit == m4.unit
 
-	assert omni_power_frame.timestamp == testTime
+	assert omni_power_frame.timestamp == testdateTime
+
+	MeterMeasurement.as_dict(omni_power_frame)=="{'Timestamp:': '2020-10-20T12:46:35', " \
+												"'Measurements': {" \
+												"'P-': {'unit': ' ', 'value':  }, " \
+												"'A+': {'unit': 'kWh', 'value': 2.15}, " \
+												"'P+': {'unit': 'kW', 'value': 0.003}, " \
+												"'A-': {'unit': 'kWh', 'value': 0.0}}, " \
+												"'Meter ID: ': '32666857'}"
+
+	MeterMeasurement.json_dump(omni_power_frame)=="{'Timestamp:': '2020-10-20T12:46:35', " \
+												"'Measurements': {" \
+												"'P-': {'unit': ' ', 'value':  }, " \
+												"'A+': {'unit': 'kWh', 'value': 2.15}, " \
+												"'P+': {'unit': 'kW', 'value': 0.003}, " \
+												"'A-': {'unit': 'kWh', 'value': 0.0}}, " \
+												"'Meter ID: ': '32666857'}"
+
 #Setup test for OmniPower class
 #@pytest.fixture()
 #def OmniFix():
@@ -82,5 +96,5 @@ def test_MeterMeasure(MeasureFix, keys):
 
 #def OmniTest(OmniFix):
 #	telegram = OmniFix
-
-
+	
+	
