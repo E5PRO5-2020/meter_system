b410177403784e309d66bb8628816de609de8de2
JakobVestergaard <failkob@gmail.com>
2020-10-27 10:19:47 +0100

    Added MyPy-test to starttest.sh

--- starttest.sh
+++ starttest.sh
@@ -2,3 +2,11 @@ coverage run -m pytest
 sleep 0.2s
 coverage report -m --omit="${PYENV_VIRTUAL_ENV}*"
 
+echo "MyPy results for /meter/"
+mypy meter
+echo "MyPy result for /test/"
+mypy test
+
+#Uncomment to test driver folder
+#echo "MyPy result for /driver/"
+#mypy driver
