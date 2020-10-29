3e4287d415784d825db8f7dfdf05efb5b4817e5c
JakobVestergaard <failkob@gmail.com>
2020-10-29 08:53:49 +0100

    Driver class ready for testing on gateway

--- test/test_DriverClass.py
+++ test/test_DriverClass.py
@@ -1,7 +1,30 @@
-import pytest
-from DriverClass import IM871A
-from main import test1
+import time
+import driver.DriverClass
+from driver.DriverClass import IM871A
 
+
+def initialize():
+    USB_port_0 = IM871A('/dev/ttyUSB0')
+    check1 = USB_port_0.reset_module()
+    # Needs time after reset before being able to setup linkmode
+    time.sleep(3)
+    check2 = USB_port_0.setup_linkmode('c1a')
+
+    if check1 & check2:
+        return True
+    else:
+        return False
+
+
+def main():
+    if __name__ == '__main__':
+        myUSB = IM871A('/dev/ttyUSB0')
+        if myUSB.reset_module():
+            # Needs time after reset before being able to setup linkmode
+            time.sleep(3)
+            myUSB.setup_linkmode('c1a')
+            while True:
+                myUSB.read_data()
 
 def test_driver():
     # Instantiate DriverClass 
