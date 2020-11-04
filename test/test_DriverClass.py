861a10ced991bf101709901c12c433cea62706e1
Janus Bo Andersen <janus@janusboandersen.dk>
2020-11-05 03:38:59 +0800

    Implement mocked tests for driver. Use skipif, to only run mocked tests on a non-Gateway machine.

--- test/test_DriverClass.py
+++ test/test_DriverClass.py
@@ -1,9 +1,16 @@
-import time
-import pytest
-import driver.DriverClass
-from driver.DriverClass import IM871A
-import os
+"""
+Tests for IM871-A driver.
+Uses mocked tests when not on Gateway.
+On Gateway, tests run using hardware peripheral.
 
+"""
+
+import pytest
+import os
+from unittest import mock
+
+# Import class to be tested
+from driver.DriverClass import IM871A
 
 # Data from DriverClass testrun
 # Raw USB data:  b'\xa5\x82\x03!D-,\x12P\x00d\x1b\x16\x8d ?\x02\xd9\xf3" Z\x06G\xe3hH\xe4\x0cE"V\x90~P\x1d\xe9\xfdl'
@@ -24,6 +31,142 @@ def input_data():
     raw_usb_data = b'\xa5\x82\x03!D-,\x12P\x00d\x1b\x16\x8d ?\x02\xd9\xf3" Z\x06G\xe3hH\xe4\x0cE"V\x90~P\x1d\xe9\xfdl'
     processed_data = 'a5820321442d2c125000641b168d203f02d9f322205a0647e36848e40c452256907e501de9fd6c'
     return raw_usb_data, processed_data
+
+
+### Janus fun and games here ###
+
+def test_vectors():
+    """
+    Build test set of raw serial data and ascii (processed) data.
+    """
+
+    # specify data as
+    # (raw bytes, raw bytes as hex in ascii, message expected in pipe)
+    t = [(b'\xa5\x82\x03!D-,\x12P\x00d\x1b\x16\x8d ?\x02\xd9\xf3" Z\x06G\xe3hH\xe4\x0cE"V\x90~P\x1d\xe9\xfdl',
+          'a5820321442d2c125000641b168d203f02d9f322205a0647e36848e40c452256907e501de9fd6c',
+          '21442d2c125000641b168d203f02d9f322205a0647e36848e40c452256907e501de9fd6c\n'), ]
+    return t
+
+
+class PatchSerial:
+    """
+    Fakes the serial.Serial object.
+    Avoids attempts of write/read operations from /dev/tty devices on local machines.
+    """
+
+    def __init__(self, read_raw_return):
+        self.read_raw_return = read_raw_return
+
+    def close(self):
+        return True
+
+    def read(self, num_bytes):
+        if num_bytes <= 10:
+            # Reading return value from setting config
+            # 0xa5 for SOF
+            # 0x01 for DEVMGMT_ID
+            # 0x04 for DEVMGMT_MSG_SET_CONFIG_RSP
+            return b'\xa5\x01\x04'
+
+        elif num_bytes > 10:
+            # Reading a raw serial bytes message
+            return self.read_raw_return
+
+    def write(self, message_bytes):
+        return True
+
+
+class PipeWriter:
+    """
+    Fakes the builtin method used to open a pipe and write to it.
+    This avoids pipes being opened on local machines.
+    """
+
+    def __init__(self):
+        self.message = str()
+        pass
+
+    def write(self, message):
+        # Store the message
+        self.message = message
+        return True
+
+    def close(self):
+        return True
+
+
+@pytest.fixture
+@mock.patch("driver.DriverClass.port.Serial", return_value=PatchSerial(test_vectors()[0][0]), autospec=True)
+def patched_driver(mock_obj):
+    """
+    Make a driver fixture where we've patched out the port.Serial dependency.
+    Use unittest to patch (bypass/override) the port.Serial-function (serial.Serial),
+    which is used when the driver tries to establish serial connection.
+    Also set up that we test on one specific test vector.
+    """
+
+    # Instantiate object
+    test_port = '/dev/ttyMonster'
+    test_pipe = test_port.split('tty')[1] + '_pipe'
+    d = IM871A(test_port)
+
+    # Return patched object
+    return d
+
+
+@pytest.mark.skipif(os.uname()[1] == 'raspberrypi', reason="Only run mocked tests when not on Gateway")
+def test_constructor_destructor(patched_driver):
+    """
+    Test construction and destruction of a driver object is OK.
+    """
+
+    d = patched_driver              # Get fixture
+
+    # Assert existence of these objects
+    assert d.Port
+    assert d.pipe == d.Port.split('tty')[1] + '_pipe'
+
+    # When object goes out of scope, destructor is called; we will force this here
+    # Expect NO exceptions or errors
+    assert d.__del__() is None
+
+
+@pytest.mark.skipif(os.uname()[1] == 'raspberrypi', reason="Only run mocked tests when not on Gateway")
+def test_close(patched_driver):
+    d = patched_driver  # Get fixture
+
+    # Closing the port returns None and doesn't raise exception
+    # d.close() -> d.IM871A.close() -> (by patch) patch_serial_instance.close()
+    assert d.close() is None
+
+
+@pytest.mark.skipif(os.uname()[1] == 'raspberrypi', reason="Only run mocked tests when not on Gateway")
+def test_setup_linkmode(patched_driver):
+    d = patched_driver  # Get fixture
+
+    # Assert that linkmode can be set with return True
+    assert d.setup_linkmode('c1a')
+
+
+@pytest.mark.skipif(os.uname()[1] == 'raspberrypi', reason="Only run mocked tests when not on Gateway")
+@mock.patch("builtins.open", return_value=PipeWriter(), autospec=True)
+def test_read_data(mock_obj, patched_driver):
+    """
+    Patch out the pipe dependency to an instance of local PipeWriter-type object that we can easily read.
+    """
+
+    d = patched_driver  # Get fixture
+
+    # Require that read_data() returns True
+    assert d.read_data()
+
+    # Require that read_data() has entered correct message into "pipe"
+    assert mock_obj.return_value.message == test_vectors()[0][2]
+
+
+
+
+### Jakob's tests here ###
 
 def test_driver(IM871A_setup, input_data):
     if os.uname()[1] == 'raspberrypi':
