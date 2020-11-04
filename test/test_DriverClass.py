e805a950259c75c1c8fa6827293a7cf9ecf5e1be
JakobVestergaard <failkob@gmail.com>
2020-11-04 12:24:16 +0100

    Fixed and split some items

--- test/test_DriverClass.py
+++ test/test_DriverClass.py
@@ -100,5 +100,5 @@ def test_driver(IM871A_setup, input_data):
 
 def test_init_open_exception(IM871A_bad_setup):
     bad_usb_port = IM871A_bad_setup
-    bad_usb_port_driver = IM871A(bad_usb_port)
-    assert IM871A(bad_usb_port) == False
+    bad_usb_port_driver = IM871A()
+    assert IM871A() == False
