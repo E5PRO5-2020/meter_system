1bc97c0faaff8af0dbed5bbe7e1adaac2e9d3609
Janus Bo Andersen <janus@janusboandersen.dk>
2020-11-06 03:23:29 +0800

    Improve asynchronous FIFO example, using system call to poll, rather than some busy waiting.

--- experimental/pipe_write.py
+++ experimental/pipe_write.py
@@ -1,24 +1,46 @@
+"""
+Example of using FIFO asynchronously. This file writes to the FIFO.
+
+- The POSIX FIFO (named pipe) uses a block buffer by default.
+- By default then, a "reader" will wait for the buffer to be full before anything is read.
+- Or, will wait for the "writer" to close the FIFO.
+- One way is to change the buffer type of the reader open(fifo, 'r', 0).
+- A better way is to just call flush on the writer side, to signify ready.
+- Then read is performed immediately.
+
+Janus, Nov. 5, 2020
+
+"""
+
 import os
 import time
+import errno
 
 
 def start_fifo_and_write_fast():
 
-    print("Open fifo and block")
+    fifo_name = "test_fifo"
+    p = os.path.abspath(fifo_name)
 
     try:
-        os.mkfifo("test_fifo")
-    except:
-        print("Didn't make FIFO, probably already exists. Trying to write to it.")
+        os.mkfifo(p)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            print("FIFO already exists.")
+        else:
+            print("Couldn't make FIFO. Exiting.")
+            exit()
 
-    fifo = open("test_fifo", 'w')
+    print("Open fifo, will block. Path: {}".format(p))
+    with open(p, 'w') as fifo:
+        for i in range(1, 11):
+            msg = "Hello #{}".format(i)
+            print("Putting message {} of 10 in FIFO: {}".format(i, msg))
+            fifo.write(msg + os.linesep)
+            fifo.flush()                    # This is important!
+            time.sleep(0.2)                 # Simulate faster writing
 
-    for i in range(0,10):
-        print("Putting message {} of 10 in FIFO".format(i))
-        fifo.write(str(i) + '\n')
-        time.sleep(0.2)
-
-    fifo.close()
+    time.sleep(2)                           # Simulate keeping FIFO open but inactive
 
 
 if __name__ == '__main__':
