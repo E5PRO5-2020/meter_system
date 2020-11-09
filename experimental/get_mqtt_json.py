56b143b14003782e5485aedda148aed93a1e48d8
Janus Bo Andersen <janus@janusboandersen.dk>
2020-11-09 20:40:18 +0800

    Update JSON example with method to get all device ids sent in one go.

--- experimental/get_mqtt_json.py
+++ experimental/get_mqtt_json.py
@@ -19,11 +19,17 @@ Message:
 """
 
 import json
+from typing import Dict, Any
 
 
-def set_json():
+def make_json() -> str:
+    """
+    Simulates a possible message from ReCalc informing the Gateway
+    about which meters to read.
+    """
+
     meter1 = {
-        "deviceId": "57686632",
+        "deviceId": "32666857",
         "manufacturerKey": "kam",
         "encryptionKey": "9A25139E3244CC2E391A8EF6B915B697",
         "manufacturerDeviceKey": "OmniPower1"
@@ -41,16 +47,25 @@ def set_json():
     return json.dumps(list_of_meters)
 
 
-def get_json(json_str):
+def get_meters_to_read(json_str: str) -> Dict[Any, Any]:
+    """
+    Simulates reading a message and parsing it as JSON->Dict
+    """
+
     return json.loads(json_str)
 
 
 if __name__ == '__main__':
 
     # Encode objects as JSON string and print to terminal
-    json_str = set_json()
+    json_str = make_json()
     print(json_str)
 
-    # Decode the JSON string, and print some data from the first of the object
-    full_obj = get_json(json_str)
+    # Decode the JSON string, and print some data from the first meter in the list object
+    full_obj = get_meters_to_read(json_str)
     print(full_obj[0]['deviceId'])
+
+    # Get all device ids
+    # Be aware that these are received big-endian from ReCalc and little-endian from wm-bus
+    device_ids = [device_data['deviceId'] for device_data in full_obj]
+    print(device_ids)
