e8c2689d137e631ebd875e36e438095e67bec880
TSerup <au575786@post.au.dk>
2020-11-12 13:50:12 +0100

    Fixed double logging

--- utils/log.py
+++ utils/log.py
@@ -7,15 +7,23 @@ def get_logger():
     Ignoring less severe messages than INFO.
     Returns a reference to a logger instance.
     """
-    # Create logger
+    # Create logger with specific name
     log = logging.getLogger('TEAM 3: ')
+    
+    # Check if handlers are already present and if so, clear them
+    if (log.hasHandlers()):
+        log.handlers.clear()
+
     # Setting logger level 
     log.setLevel(logging.INFO)
+    
     # Create handler addressed to syslog
     handler = logging.handlers.SysLogHandler(address = '/dev/log')
+    
     # Create and add formatter to handler
     formatter = logging.Formatter('%(name)s %(levelname)s: %(message)s')
     handler.setFormatter(formatter)
+    
     # Add handler to logger
     log.addHandler(handler)
 
